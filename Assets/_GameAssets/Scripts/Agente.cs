2c412561a559ac3f1278f1678babafc4bdef0bdf
Fernando Paniagua <fernando.paniagua@gmail.com>
2020-10-27 14:07:48 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Agente.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agente : MonoBehaviour
+{
+    public Transform[] targetPoints;
+    NavMeshAgent nma;
+    int targetIndex = 0;
+    private void Awake()
+    {
+        nma = GetComponent<NavMeshAgent>();
+    }
+    void Start()
+    {
+        nma.SetDestination(targetPoints[targetIndex].position);
+    }
+    private void Update()
+    {
+        if (nma.hasPath)
+        {
+            if (nma.remainingDistance < 1)
+            {
+                targetIndex++;
+                //targetIndex = targetIndex == targetPoints.Length ? 0 : targetIndex;
+                if (targetIndex == targetPoints.Length)
+                {
+                    targetIndex = 0;
+                }
+                nma.SetDestination(targetPoints[targetIndex].position);
+            }
+        }
+    }
+}
