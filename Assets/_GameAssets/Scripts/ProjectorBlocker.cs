6ebacec3316241a1f40af8a8fdb53368d65de3a1
Fernando Paniagua <fernando.paniagua@gmail.com>
2020-11-09 09:52:56 +0100

    Incorporacion Proyectores
    Incorporacion proyectores para sombras y cono de vision

---
+++ Assets/_GameAssets/Scripts/ProjectorBlocker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectorBlocker : MonoBehaviour
+{
+    Quaternion rotacionInicial;
+    Quaternion rotacion;
+
+    private void Start()
+    {
+        rotacionInicial = transform.rotation;
+    }
+
+    void Update()
+    {
+        rotacion = Quaternion.Euler(rotacionInicial.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        transform.rotation = rotacion;
+    }
+}
